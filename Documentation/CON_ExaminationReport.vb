5973bc7a06991a70f519c13409c189a9597b188b
Sami Norling <sami.norling@gmail.com>
2020-10-12 14:46:51 -0400

    Update CON_ExaminationReport

--- Documentation/CON_ExaminationReport.vb
+++ Documentation/CON_ExaminationReport.vb
@@ -45,9 +45,21 @@ End With
    connection.Open "DSN=EMu Conservation;"
    '
    ' This code cycles through each table we have added to the Word document in order to populate
-   ' it with data. In this example we have only one table
+   ' it with data. 
    For i = 1 To ActiveDocument.Tables.Count
        With ActiveDocument.Tables(i)
+           '
+           ' Insert Multimedia
+            If .Columns.Count = 1 Then
+               key = .Cell(1, 1).Range.Text
+               key = Trim(Left(key, Len(key) - 2))
+               .Cell(1, 1).Range.Text = ""
+                recordset.Open "select * from Media.csv where Objects_key = " & key, connection
+                apath = recordset("Multimedia")
+                .Cell(1, 1).Range.InlineShapes.AddPicture FileName:=apath, _
+                LinkToFile:=False, SaveWithDocument:=True
+                recordset.Close
+            End If
            '
            ' Copy and then modify the following "If" section of code (between the If and
            ' End If statements) for each .csv file generated.
@@ -71,7 +83,118 @@ End With
                    row = row + 1
                    .Rows.Add
                    .Cell(row, 1).Range.Text = recordset("CreRole") & ":"
-                   .Cell(row, 2).Range.Text = recordset("CreCreatorAfterFollower") & " " & recordset("NamFullName")
+                   .Cell(row, 2).Range.Text = Trim(recordset("CreCreatorAfterFollower") & " " & recordset("NamFullName"))
+                   recordset.MoveNext
+               Wend
+               recordset.Close
+               '
+               ' Remove the row containing the key so that it doesn't display
+               ' in the mail merge report
+               .Rows(1).Select
+               Selection.Rows.Delete
+   
+            ' Remove the table borders
+            .Borders.Enable = False
+   
+           End If
+           '
+           ' Copy and then modify the following "If" section of code (between the If and
+           ' End If statements) for each .csv file generated.
+           If InStr(.Cell(1, 1).Range.Text, "CON#:") = 1 Then
+               '
+               ' Identify the key - in this case it is located in row 1, column 2
+               key = .Cell(1, 2).Range.Text
+               key = Trim(Left(key, Len(key) - 2))
+               '
+               ' We want to display the CON#.
+               ' We use the following code to add a row dynamically for each record.
+               row = 1
+               '
+               ' You would replace the name of the .csv file and key as required
+               ' by your report.
+               ' In this example the .csv file is called econserv.csv
+               ' and the key is econservation_key
+               recordset.Open "select * from econserv.csv where econservation_key = " & key, connection
+               While Not recordset.EOF
+                   row = row + 1
+                   .Rows.Add
+                   .Cell(row, 1).Range.Text = "CON#:"
+                   .Cell(row, 2).Range.Text = recordset("ReqConservationIdentifier")
+                   recordset.MoveNext
+               Wend
+               recordset.Close
+               '
+               ' Remove the row containing the key so that it doesn't display
+               ' in the mail merge report
+               .Rows(1).Select
+               Selection.Rows.Delete
+   
+            ' Remove the table borders
+            .Borders.Enable = False
+   
+           End If
+           '
+           ' Copy and then modify the following "If" section of code (between the If and
+           ' End If statements) for each .csv file generated.
+           If InStr(.Cell(1, 1).Range.Text, "Conservators:") = 1 Then
+               '
+               ' Identify the key - in this case it is located in row 1, column 2
+               key = .Cell(1, 2).Range.Text
+               key = Trim(Left(key, Len(key) - 2))
+               '
+               ' We want to display the names of all associated conservators.
+               ' Because the number of attached Conservs records is unknown, we use the
+               ' following code to add a row dynamically for each record.
+               row = 1
+               '
+               ' You would replace the name of the .csv file and key as required
+               ' by your report.
+               ' In this example the .csv file is called Conservs.csv
+               ' and the key is econservation_key
+               recordset.Open "select * from Conservs.csv where econservation_key = " & key, connection
+               While Not recordset.EOF
+                   row = row + 1
+                   .Rows.Add
+                   .Cell(row, 2).Range.Text = Replace(recordset("SummaryData"), "IMA - ", "")
+                   recordset.MoveNext
+               Wend
+               recordset.Close
+               '
+               ' Remove the row containing the key so that it doesn't display
+               ' in the mail merge report
+               .Rows(1).Select
+               Selection.Rows.Delete
+               '
+               ' Add "Conservators:"
+               .Cell(1, 1).Range.Text = "Conservators:"
+   
+            ' Remove the table borders
+            .Borders.Enable = False
+   
+           End If
+           '
+           ' Copy and then modify the following "If" section of code (between the If and
+           ' End If statements) for each .csv file generated.
+           If InStr(.Cell(1, 1).Range.Text, "Completion Date:") = 1 Then
+               '
+               ' Identify the key - in this case it is located in row 1, column 2
+               key = .Cell(1, 2).Range.Text
+               key = Trim(Left(key, Len(key) - 2))
+               '
+               ' We want to display the completion date.
+               ' We use the following code to add a row dynamically for each record.
+               row = 1
+               '
+               ' You would replace the name of the .csv file and key as required
+               ' by your report.
+               ' In this example the .csv file is called econserv.csv
+               ' and the key is econservation_key
+               recordset.Open "select * from econserv.csv where econservation_key = " & key, connection
+               While Not recordset.EOF
+                   row = row + 1
+                   .Rows.Add
+                   .Cell(row, 1).Range.Text = "Completion Date:"
+                   .Cell(row, 2).Range.Text = recordset("ReqActualCompletionDate")
                    recordset.MoveNext
                Wend
                recordset.Close
