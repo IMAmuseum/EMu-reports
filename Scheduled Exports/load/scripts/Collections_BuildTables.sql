d5e3932743f31d37600e94048df9ddcc7533de4e
Sami Norling <sami.norling@gmail.com>
2022-07-11 20:08:12 -0400

    Clean up sample db files, remove all "null" strings
    PORTAL-44

---
+++ Scheduled Exports/load/scripts/Collections_BuildTables.sql
@@ -0,0 +1,499 @@
+USE CollectionsTest
+GO
+
+CREATE TABLE [objects] (
+  [object_id] int PRIMARY KEY NOT NULL IDENTITY(1, 1),
+  [emu_irn] int NOT NULL,
+  [dagwood_id] int,
+  [publish] bit NOT NULL,
+  [status] nvarchar(255),
+  [accession_number] nvarchar(255),
+  [previous_id] nvarchar(255),
+  [date_accessioned_year] int,
+  [date_accessioned_month] int,
+  [date_accessioned_day] int,
+  [title] nvarchar(255),
+  [series_title] nvarchar(255),
+  [portfolio_title] nvarchar(255),
+  [date_created] nvarchar(255),
+  [date_created_earliest] int,
+  [date_created_latest] int,
+  [date_designed] nvarchar(255),
+  [date_designed_earliest] int,
+  [date_designed_latest] int,
+  [period] nvarchar(255),
+  [dynasty] nvarchar(255),
+  [record_type] nvarchar(255) NOT NULL,
+  [medium_support] nvarchar(255),
+  [technique] nvarchar(255),
+  [style] nvarchar(255),
+  [mark_description] nvarchar(255),
+  [dimensions] nvarchar(255),
+  [credit_line] nvarchar(255),
+  [provenance] varchar(max),
+  [department] int,
+  [on_view] bit NOT NULL,
+  [location_id] int,
+  [parent_irn] int,
+  [deaccession_method] nvarchar(255),
+  [deaccession_date_year] int,
+  [deaccession_date_month] int,
+  [deaccession_date_day] int,
+  [deaccession_notes] varchar(max),
+  [recipient] int,
+  [transfer_notes] varchar(max),
+  [date_modified] date NOT NULL
+)
+GO
+
+CREATE TABLE [flags] (
+  [flag_id] int PRIMARY KEY NOT NULL IDENTITY(1, 1),
+  [flag] nvarchar(255) UNIQUE
+)
+GO
+
+CREATE TABLE [object_flags] (
+  [object_id] int NOT NULL,
+  [flag_id] int NOT NULL,
+  [sort_order] int NOT NULL,
+  PRIMARY KEY ([object_id], [sort_order])
+)
+GO
+
+CREATE TABLE [alt_titles] (
+  [object_id] int NOT NULL,
+  [alt_title] nvarchar(255),
+  [sort_order] int NOT NULL,
+  PRIMARY KEY ([object_id], [sort_order])
+)
+GO
+
+CREATE TABLE [object_creators] (
+  [object_id] int NOT NULL,
+  [party_id] int NOT NULL,
+  [attribution] nvarchar(255),
+  [role] nvarchar(255),
+  [sort_order] int NOT NULL,
+  PRIMARY KEY ([object_id], [sort_order])
+)
+GO
+
+CREATE TABLE [object_cultures] (
+  [object_id] int NOT NULL,
+  [attribution] nvarchar(255),
+  [culture] nvarchar(255),
+  [nationality] nvarchar(255),
+  [sort_order] int NOT NULL,
+  PRIMARY KEY ([object_id], [sort_order])
+)
+GO
+
+CREATE TABLE [object_places] (
+  [object_id] int NOT NULL,
+  [country] nvarchar(255),
+  [state] nvarchar(255),
+  [district] nvarchar(255),
+  [city] nvarchar(255),
+  [sort_order] int NOT NULL,
+  PRIMARY KEY ([object_id], [sort_order])
+)
+GO
+
+CREATE TABLE [mediums] (
+  [medium_id] int PRIMARY KEY NOT NULL IDENTITY(1, 1),
+  [medium] nvarchar(255) UNIQUE NOT NULL
+)
+GO
+
+CREATE TABLE [object_mediums] (
+  [object_id] int NOT NULL,
+  [medium_id] int NOT NULL,
+  [sort_order] int NOT NULL,
+  PRIMARY KEY ([object_id], [sort_order])
+)
+GO
+
+CREATE TABLE [supports] (
+  [support_id] int PRIMARY KEY NOT NULL IDENTITY(1, 1),
+  [support] nvarchar(255) UNIQUE NOT NULL
+)
+GO
+
+CREATE TABLE [object_supports] (
+  [object_id] int NOT NULL,
+  [support_id] int NOT NULL,
+  [sort_order] int NOT NULL,
+  PRIMARY KEY ([object_id], [sort_order])
+)
+GO
+
+CREATE TABLE [types] (
+  [type_id] int PRIMARY KEY NOT NULL IDENTITY(1, 1),
+  [type] nvarchar(255) UNIQUE NOT NULL
+)
+GO
+
+CREATE TABLE [object_types] (
+  [object_id] int NOT NULL,
+  [type_id] int NOT NULL,
+  [sort_order] int NOT NULL,
+  PRIMARY KEY ([object_id], [sort_order])
+)
+GO
+
+CREATE TABLE [mark_types] (
+  [mark_type_id] int PRIMARY KEY NOT NULL,
+  [mark_type] nvarchar(255) UNIQUE NOT NULL
+)
+GO
+
+CREATE TABLE [object_marks] (
+  [object_id] int NOT NULL,
+  [mark_type_id] int NOT NULL,
+  [sort_order] int NOT NULL,
+  PRIMARY KEY ([object_id], [sort_order])
+)
+GO
+
+CREATE TABLE [object_printers] (
+  [object_id] int NOT NULL,
+  [party_id] int NOT NULL,
+  [sort_order] int NOT NULL,
+  PRIMARY KEY ([object_id], [sort_order])
+)
+GO
+
+CREATE TABLE [object_publishers] (
+  [object_id] int NOT NULL,
+  [party_id] int NOT NULL,
+  [sort_order] int NOT NULL,
+  PRIMARY KEY ([object_id], [sort_order])
+)
+GO
+
+CREATE TABLE [object_dimensions] (
+  [object_id] int NOT NULL,
+  [type] nvarchar(255),
+  [height] decimal(18,4),
+  [width] decimal(18,4),
+  [depth] decimal(18,4),
+  [diameter] decimal(18,4),
+  [length_unit] nvarchar(255),
+  [weight] decimal(18,4),
+  [weight_unit] nvarchar(255),
+  [notes] nvarchar(255),
+  [sort_order] int NOT NULL,
+  PRIMARY KEY ([object_id], [sort_order])
+)
+GO
+
+CREATE TABLE [rights_uris] (
+  [rights_uri_id] int PRIMARY KEY NOT NULL IDENTITY(1, 1),
+  [type] nvarchar(255) NOT NULL,
+  [uri] nvarchar(255) UNIQUE NOT NULL
+)
+GO
+
+CREATE TABLE [object_rights_uris] (
+  [object_id] int NOT NULL,
+  [rights_uri_id] int NOT NULL,
+  [sort_order] int NOT NULL,
+  PRIMARY KEY ([object_id], [sort_order])
+)
+GO
+
+CREATE TABLE [departments] (
+  [department_id] int PRIMARY KEY NOT NULL IDENTITY(1, 1),
+  [department] nvarchar(255) UNIQUE NOT NULL
+)
+GO
+
+CREATE TABLE [locations] (
+  [location_id] int PRIMARY KEY NOT NULL IDENTITY(1, 1),
+  [emu_irn] int NOT NULL,
+  [publish] bit NOT NULL,
+  [code] nvarchar(255),
+  [level_1] nvarchar(255),
+  [level_2] nvarchar(255),
+  [level_3] nvarchar(255),
+  [level_4] nvarchar(255),
+  [level_5] nvarchar(255),
+  [level_6] nvarchar(255),
+  [level_7] nvarchar(255),
+  [level_8] nvarchar(255),
+  [date_modified] date NOT NULL
+)
+GO
+
+CREATE TABLE [related_objects] (
+  [object_id_1] int NOT NULL,
+  [object_id_2] int NOT NULL,
+  [relationship] nvarchar(255),
+  [sort_order] int NOT NULL,
+  PRIMARY KEY ([object_id_1], [sort_order])
+)
+GO
+
+CREATE TABLE [object_guids] (
+  [object_id] int NOT NULL,
+  [preferred] bit NOT NULL,
+  [type] nvarchar(255) NOT NULL,
+  [guid] nvarchar(255) NOT NULL,
+  [sort_order] int NOT NULL,
+  PRIMARY KEY ([object_id], [sort_order])
+)
+GO
+
+CREATE TABLE [narratives] (
+  [narrative_id] int PRIMARY KEY NOT NULL IDENTITY(1, 1),
+  [emu_irn] int NOT NULL,
+  [publish] bit NOT NULL,
+  [title] nvarchar(255),
+  [purpose] nvarchar(255),
+  [date_year] int,
+  [date_month] int,
+  [date_day] int,
+  [narrative] varchar(max) NOT NULL,
+  [date_modified] date NOT NULL
+)
+GO
+
+CREATE TABLE [narrative_authors] (
+  [narrative_id] int NOT NULL,
+  [party_id] int NOT NULL,
+  [sort_order] int NOT NULL,
+  PRIMARY KEY ([narrative_id], [sort_order])
+)
+GO
+
+CREATE TABLE [narratives_objects] (
+  [narrative_id] int NOT NULL,
+  [object_id] int NOT NULL,
+  [sort_order] int NOT NULL,
+  PRIMARY KEY ([narrative_id], [sort_order])
+)
+GO
+
+CREATE TABLE [narrative_events] (
+  [narrative_id] int NOT NULL,
+  [event_id] int NOT NULL,
+  [sort_order] int NOT NULL,
+  PRIMARY KEY ([narrative_id], [sort_order])
+)
+GO
+
+CREATE TABLE [narrative_parties] (
+  [narrative_id] int NOT NULL,
+  [party_id] int NOT NULL,
+  [sort_order] int NOT NULL,
+  PRIMARY KEY ([narrative_id], [sort_order])
+)
+GO
+
+CREATE TABLE [parties] (
+  [party_id] int PRIMARY KEY NOT NULL IDENTITY(1, 1),
+  [emu_irn] int NOT NULL,
+  [party_type] nvarchar(255),
+  [full_name] nvarchar(255),
+  [title] nvarchar(255),
+  [first_name] nvarchar(255),
+  [middle_name] nvarchar(255),
+  [last_name] nvarchar(255),
+  [suffix] nvarchar(255),
+  [gender] nvarchar(255),
+  [nationality] nvarchar(255),
+  [birth_date] nvarchar(255),
+  [birth_place] nvarchar(255),
+  [death_date] nvarchar(255),
+  [death_place] nvarchar(255),
+  [organization_name] nvarchar(255),
+  [commencement_date] nvarchar(255),
+  [completion_date] nvarchar(255),
+  [collaboration_name] nvarchar(255),
+  [acronym] nvarchar(255),
+  [date_modified] date NOT NULL
+)
+GO
+
+CREATE TABLE [party_other_names] (
+  [party_id] int NOT NULL,
+  [other_name] nvarchar(255) NOT NULL,
+  [sort_order] int NOT NULL,
+  PRIMARY KEY ([party_id], [sort_order])
+)
+GO
+
+CREATE TABLE [party_identities] (
+  [party_id] int NOT NULL,
+  [identity] nvarchar(255) NOT NULL,
+  [sort_order] int NOT NULL,
+  PRIMARY KEY ([party_id], [sort_order])
+)
+GO
+
+CREATE TABLE [collaborators] (
+  [party_id] int NOT NULL,
+  [collaborator_id] int NOT NULL,
+  [sort_order] int NOT NULL,
+  PRIMARY KEY ([party_id], [sort_order])
+)
+GO
+
+CREATE TABLE [roles] (
+  [role_id] int PRIMARY KEY NOT NULL IDENTITY(1, 1),
+  [role] nvarchar(255) UNIQUE NOT NULL
+)
+GO
+
+CREATE TABLE [party_roles] (
+  [party_id] int NOT NULL,
+  [role_id] int,
+  [sort_order] int NOT NULL,
+  PRIMARY KEY ([party_id], [sort_order])
+)
+GO
+
+CREATE TABLE [party_guids] (
+  [party_id] int NOT NULL,
+  [preferred] bit NOT NULL,
+  [type] nvarchar(255) NOT NULL,
+  [guid] nvarchar(255) NOT NULL,
+  [sort_order] int NOT NULL,
+  PRIMARY KEY ([party_id], [sort_order])
+)
+GO
+
+CREATE TABLE [events] (
+  [event_id] int PRIMARY KEY NOT NULL IDENTITY(1, 1),
+  [emu_irn] int NOT NULL
+)
+GO
+
+ALTER TABLE [objects] ADD FOREIGN KEY ([department]) REFERENCES [departments] ([department_id])
+GO
+
+ALTER TABLE [objects] ADD FOREIGN KEY ([location_id]) REFERENCES [locations] ([location_id])
+GO
+
+ALTER TABLE [objects] ADD FOREIGN KEY ([parent_irn]) REFERENCES [objects] ([object_id])
+GO
+
+ALTER TABLE [object_flags] ADD FOREIGN KEY ([object_id]) REFERENCES [objects] ([object_id])
+GO
+
+ALTER TABLE [object_flags] ADD FOREIGN KEY ([flag_id]) REFERENCES [flags] ([flag_id])
+GO
+
+ALTER TABLE [alt_titles] ADD FOREIGN KEY ([object_id]) REFERENCES [objects] ([object_id])
+GO
+
+ALTER TABLE [object_creators] ADD FOREIGN KEY ([object_id]) REFERENCES [objects] ([object_id])
+GO
+
+ALTER TABLE [object_creators] ADD FOREIGN KEY ([party_id]) REFERENCES [parties] ([party_id])
+GO
+
+ALTER TABLE [object_cultures] ADD FOREIGN KEY ([object_id]) REFERENCES [objects] ([object_id])
+GO
+
+ALTER TABLE [object_places] ADD FOREIGN KEY ([object_id]) REFERENCES [objects] ([object_id])
+GO
+
+ALTER TABLE [object_mediums] ADD FOREIGN KEY ([object_id]) REFERENCES [objects] ([object_id])
+GO
+
+ALTER TABLE [object_mediums] ADD FOREIGN KEY ([medium_id]) REFERENCES [mediums] ([medium_id])
+GO
+
+ALTER TABLE [object_supports] ADD FOREIGN KEY ([object_id]) REFERENCES [objects] ([object_id])
+GO
+
+ALTER TABLE [object_supports] ADD FOREIGN KEY ([support_id]) REFERENCES [supports] ([support_id])
+GO
+
+ALTER TABLE [object_types] ADD FOREIGN KEY ([object_id]) REFERENCES [objects] ([object_id])
+GO
+
+ALTER TABLE [object_types] ADD FOREIGN KEY ([type_id]) REFERENCES [types] ([type_id])
+GO
+
+ALTER TABLE [object_marks] ADD FOREIGN KEY ([object_id]) REFERENCES [objects] ([object_id])
+GO
+
+ALTER TABLE [object_marks] ADD FOREIGN KEY ([mark_type_id]) REFERENCES [mark_types] ([mark_type_id])
+GO
+
+ALTER TABLE [object_printers] ADD FOREIGN KEY ([object_id]) REFERENCES [objects] ([object_id])
+GO
+
+ALTER TABLE [object_printers] ADD FOREIGN KEY ([party_id]) REFERENCES [parties] ([party_id])
+GO
+
+ALTER TABLE [object_publishers] ADD FOREIGN KEY ([object_id]) REFERENCES [objects] ([object_id])
+GO
+
+ALTER TABLE [object_publishers] ADD FOREIGN KEY ([party_id]) REFERENCES [parties] ([party_id])
+GO
+
+ALTER TABLE [object_dimensions] ADD FOREIGN KEY ([object_id]) REFERENCES [objects] ([object_id])
+GO
+
+ALTER TABLE [object_rights_uris] ADD FOREIGN KEY ([object_id]) REFERENCES [objects] ([object_id])
+GO
+
+ALTER TABLE [object_rights_uris] ADD FOREIGN KEY ([rights_uri_id]) REFERENCES [rights_uris] ([rights_uri_id])
+GO
+
+ALTER TABLE [related_objects] ADD FOREIGN KEY ([object_id_1]) REFERENCES [objects] ([object_id])
+GO
+
+ALTER TABLE [related_objects] ADD FOREIGN KEY ([object_id_2]) REFERENCES [objects] ([object_id])
+GO
+
+ALTER TABLE [object_guids] ADD FOREIGN KEY ([object_id]) REFERENCES [objects] ([object_id])
+GO
+
+ALTER TABLE [narrative_authors] ADD FOREIGN KEY ([narrative_id]) REFERENCES [narratives] ([narrative_id])
+GO
+
+ALTER TABLE [narrative_authors] ADD FOREIGN KEY ([party_id]) REFERENCES [parties] ([party_id])
+GO
+
+ALTER TABLE [narratives_objects] ADD FOREIGN KEY ([narrative_id]) REFERENCES [narratives] ([narrative_id])
+GO
+
+ALTER TABLE [narratives_objects] ADD FOREIGN KEY ([object_id]) REFERENCES [objects] ([object_id])
+GO
+
+ALTER TABLE [narrative_events] ADD FOREIGN KEY ([narrative_id]) REFERENCES [narratives] ([narrative_id])
+GO
+
+ALTER TABLE [narrative_events] ADD FOREIGN KEY ([event_id]) REFERENCES [events] ([event_id])
+GO
+
+ALTER TABLE [narrative_parties] ADD FOREIGN KEY ([narrative_id]) REFERENCES [narratives] ([narrative_id])
+GO
+
+ALTER TABLE [narrative_parties] ADD FOREIGN KEY ([party_id]) REFERENCES [parties] ([party_id])
+GO
+
+ALTER TABLE [party_other_names] ADD FOREIGN KEY ([party_id]) REFERENCES [parties] ([party_id])
+GO
+
+ALTER TABLE [party_identities] ADD FOREIGN KEY ([party_id]) REFERENCES [parties] ([party_id])
+GO
+
+ALTER TABLE [collaborators] ADD FOREIGN KEY ([party_id]) REFERENCES [parties] ([party_id])
+GO
+
+ALTER TABLE [collaborators] ADD FOREIGN KEY ([collaborator_id]) REFERENCES [parties] ([party_id])
+GO
+
+ALTER TABLE [party_roles] ADD FOREIGN KEY ([party_id]) REFERENCES [parties] ([party_id])
+GO
+
+ALTER TABLE [party_roles] ADD FOREIGN KEY ([role_id]) REFERENCES [roles] ([role_id])
+GO
+
+ALTER TABLE [party_guids] ADD FOREIGN KEY ([party_id]) REFERENCES [parties] ([party_id])
+GO
---
+++ Scheduled Exports/load/scripts/Collections_DropTables.sql
@@ -0,0 +1,38 @@
+USE CollectionsTest
+GO
+
+DROP TABLE [object_flags];
+DROP TABLE [object_creators];
+DROP TABLE [object_types];
+DROP TABLE [object_supports];
+DROP TABLE [object_mediums];
+DROP TABLE [object_marks];
+DROP TABLE [object_rights_uris];
+DROP TABLE [narrative_events];
+DROP TABLE [narrative_authors];
+DROP TABLE [narrative_parties];
+DROP TABLE [narrative_objects];
+DROP TABLE [party_other_names];
+DROP TABLE [party_identities];
+DROP TABLE [party_collaborators];
+DROP TABLE [party_guids];
+DROP TABLE [party_roles];
+DROP TABLE [alt_titles];
+DROP TABLE [object_cultures];
+DROP TABLE [object_places];
+DROP TABLE [object_dimensions];
+DROP TABLE [related_objects];
+DROP TABLE [object_guids];
+DROP TABLE [flags];
+DROP TABLE [types];
+DROP TABLE [supports];
+DROP TABLE [mediums];
+DROP TABLE [mark_types];
+DROP TABLE [rights_uris];
+DROP TABLE [events];
+DROP TABLE [roles];
+DROP TABLE [narratives];
+DROP TABLE [parties];
+DROP TABLE [objects];
+DROP TABLE [departments];
+DROP TABLE [locations];
